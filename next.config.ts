48ec4c9887b91d76fa140f6a1c00403f35ff0743
mnsinri <khj.mkyohei727@gmail.com>
2025-07-24 16:27:28 +0000

    Initial commit

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+	webpack: (config) => {
+		config.watchOptions = {
+			poll: 1000,
+			aggregateTimeout: 1000,
+		};
+		return config;
+	},
+};
+
+export default nextConfig;
